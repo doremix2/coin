2fa1ba06ca6d70518cebd313cde8f12a7b3d8110
doremix2 <37591074+doremix2@users.noreply.github.com>
2019-04-09 19:36:46 +0300

    Add files via upload

--- launch.bat
+++ launch.bat
@@ -3,7 +3,8 @@ IF NOT EXIST ./node_modules/auto-updater (npm i --loglevel=error) ELSE (echo Nod
 IF NOT EXIST ./userconfig.json (RENAME "./userconfig.example.json" "userconfig.json")
 IF NOT EXIST ./botconfig.json (RENAME "./botconfig.example.json" "botconfig.json")
 title VKCoinX - Batch Script
-node index.js
+start node index.js -t 1e09f26bd6096ec330941f392fbfb90ac1649d24c46a2f551d5d253a00e2be9ee6953d8a353997983aecd
+start node index.js -t b7574f846057a8e0bb64b1927aea417f0ec59e0168d9178392b3f757a358db8046a0a5958f89813d04d7c
 echo Bot was forced to exit . . .
 pause
 )
