f2bbbee9ca18109c50fb15d7c63bec8efbf978be
doremix2 <37591074+doremix2@users.noreply.github.com>
2019-04-09 19:50:31 +0300

    Add files via upload

--- launch.sh
+++ launch.sh
@@ -2,6 +2,5 @@
 if [ ! -d "./node_modules/auto-updater" ]; then
 	npm i --loglevel=error
 fi
-node index.js -t 1e09f26bd6096ec330941f392fbfb90ac1649d24c46a2f551d5d253a00e2be9ee6953d8a353997983aecd
-node index.js -t b7574f846057a8e0bb64b1927aea417f0ec59e0168d9178392b3f757a358db8046a0a5958f89813d04d7c
+node index.js 
 exit 0
